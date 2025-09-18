dd11b2ab416c76bc985eda50c59d0b08f20cf32f
SivaSubramanyaBharathy <sivajob1212@gmail.com>
2025-09-19 00:16:35 +0530

    Re-add frontend as normal folder

---
+++ to-do-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>To-list-application yet to be do</h1>
+      
+    </div>
+  );
+}
+
+export default App;
