6fb1604c717a5825b144d9bd883365b1746793d7
SivaSubramanyaBharathy <sivajob1212@gmail.com>
2025-09-23 19:37:20 +0530

    Login and Register Page

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+// import Login from './components/Login';
 
 function App() {
   return (
-    <div className="app">
-      <h1>To-list-application yet to be do</h1>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register/>}/>
+        {/* <Route path='/login' element={<Login/>}/> */}
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
